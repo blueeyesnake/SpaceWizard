e42bdc7a6e5665f277babf7b821566448267fa75
blueeyesnake <cullen.j.phillips@gmail.com>
2022-03-26 16:50:59 -0500

    commit4

---
+++ House Flipper V2/Assets/HardStartButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HardStartButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartHard()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
