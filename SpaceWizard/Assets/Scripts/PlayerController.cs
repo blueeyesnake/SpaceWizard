1bf90fee01b039b709b9ecdd58fc1c7d4bbbd9c6
blueeyesnake <cullen.j.phillips@gmail.com>
2022-03-28 18:53:59 -0500

    character look direction
    made the player character look at the mouse

--- SpaceWizard/Assets/Scripts/PlayerController.cs
+++ SpaceWizard/Assets/Scripts/PlayerController.cs
@@ -11,9 +11,13 @@ public class PlayerController : MonoBehaviour
     private Vector3 moveInput;
     private Vector3 moveVelocity;
     // Start is called before the first frame update
+
+    private Camera mainCamera;
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody>();
+        //finds camera in world
+        mainCamera = FindObjectOfType<Camera>();
     }
 
     // Update is called once per frame
@@ -21,7 +25,20 @@ public class PlayerController : MonoBehaviour
     {
         moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
         moveVelocity = moveInput * moveSpeed;
+        //makes character look at mouse
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        //makes new plane at default position
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+        //if the ray hits something in the world, returns true
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            //just for debugging where the player should be looking in the scene
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
 
+            transform.LookAt(pointToLook);
+        }
     }
     //instead of once per frame, fixed update happens at a set time
     private void FixedUpdate()
