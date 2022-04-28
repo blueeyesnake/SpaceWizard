a82b067708c1ebf03217a8e8261dc13e62337c4a
blueeyesnake <cullen.j.phillips@gmail.com>
2022-04-27 23:53:07 -0500

    added damage to ice spell

---
+++ SpaceWizard/Assets/Scripts/IceDamage.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceDamage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int damageToGive;
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            other.gameObject.GetComponent<RadialBulletController>().TakeDamage(damageToGive);
+        }
+    }
+}
